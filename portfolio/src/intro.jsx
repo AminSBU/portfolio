d4855cba5269021c2e990c90a5c1d9ae19e03270
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-08-19 23:41:59 +0330

    use image to show

--- portfolio/src/intro.jsx
+++ portfolio/src/intro.jsx
@@ -3,7 +3,17 @@ import './intro.css'
 function Intro()
 {
     return(<>
-        <h1 className="PortfolioName">Amin Bakhshoodeh</h1>
+        <div className='container'>
+            <div className='left'>
+                <h1 className="PortfolioName">Amin Bakhshoodeh</h1>
+                <p className='paragraphText'>Embedded Software developer in MAPNA (MECO) as developing EV Chargers (240KW) CCS, GB, CHAdeMO</p>
+            </div>
+            <div className='right'>
+                <img src='src/img/avatar.jpeg' className='imageClass'></img>
+            </div>
+        </div>
+
+        
     </>);
 }
 export default Intro;
