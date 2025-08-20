9f149bfd13022379e0ee7d3616c8aa131115b162
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-08-20 23:59:52 +0330

    add a button for all cv

--- portfolio/src/intro.jsx
+++ portfolio/src/intro.jsx
@@ -6,10 +6,21 @@ function Intro()
         <div className='container'>
             <div className='left'>
                 <h1 className="PortfolioName">Amin Bakhshoodeh</h1>
-                <p className='paragraphText'>Embedded Software developer in MAPNA (MECO) as developing EV Chargers (240KW) CCS, GB, CHAdeMO</p>
+                <p className='paragraphText'>As an experienced Embedded Software Engineer 
+                                            with over 8 years in electronic design and engineering, 
+                                            I have a proven track record of delivering innovative 
+                                            solutions in the electrical and electronic manufacturing industry. 
+                                            My strong background in C/C++, STM32 (Cortex-M4, Cortex-M7), ARM Architecture, 
+                                            and Printed Circuit Board (PCB) Design allows me to contribute effectively 
+                                            to diverse projects, enhancing system performance and functionality.</p>
+
+                <button className='resumeBtn'>See CV !</button>
             </div>
             <div className='right'>
                 <img src='src/img/avatar.jpeg' className='imageClass'></img>
+            </div>
+            <div className='allresumeBtn'>
+                
             </div>
         </div>
 
