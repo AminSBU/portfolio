2c2d45b963d04c377dab62281fe8668cac4c15a0
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-08-24 21:43:25 +0330

    user BrowserRoute, Route and switch

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Intro from './intro.jsx'
+import {
+  BrowserRouter,
+  Router,
+  Switch
+} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Intro />
+      <BrowserRouter>
+        <Switch>
+          <Router path="/">
+            <Intro />
+          </Router>
+        </Switch>
+      </BrowserRouter>
     </>
   )
 }
