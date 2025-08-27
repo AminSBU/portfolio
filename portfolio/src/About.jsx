e9a3c3625dff74d9d71aed1ea029e28053e35584
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-08-27 19:40:25 +0330

    add about.css and transfer about from intro file

--- portfolio/src/About.jsx
+++ portfolio/src/About.jsx
@@ -1,6 +1,27 @@
+import './about.css'
+
 export default function About()
 {
     return(<>
-        <p>about</p>
+        <div className='container'>
+            <div className='left'>
+                <h1 className="PortfolioName">Amin Bakhshoodeh</h1>
+                <p className='paragraphText'>As an experienced Embedded Software Engineer 
+                                            with over 8 years in electronic design and engineering, 
+                                            I have a proven track record of delivering innovative 
+                                            solutions in the electrical and electronic manufacturing industry. 
+                                            My strong background in C/C++, STM32 (Cortex-M4, Cortex-M7), ARM Architecture, 
+                                            and Printed Circuit Board (PCB) Design allows me to contribute effectively 
+                                            to diverse projects, enhancing system performance and functionality.</p>
+
+                <button className='resumeBtn'>See CV !</button>
+            </div>
+            <div className='right'>
+                <img src='src/img/avatar.jpeg' className='imageClass'></img>
+            </div>
+            <div className='allresumeBtn'>
+                
+            </div>
+        </div>
     </>);
 }
