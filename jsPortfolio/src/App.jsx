59dfb8257f31dcca0eead642c7b4ed08aa7115f4
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-11-11 23:01:24 +0330

    create Home, About and contact page, some modification in App.jsx

--- jsPortfolio/src/App.jsx
+++ jsPortfolio/src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header.jsx'
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
 
 function App() {
   return (
