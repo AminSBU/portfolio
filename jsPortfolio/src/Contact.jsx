eba33816c6c804877406986f2f1eff756d7aa2af
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-11-16 17:21:30 +0330

    name and email input were added in form contact

--- jsPortfolio/src/Contact.jsx
+++ jsPortfolio/src/Contact.jsx
@@ -2,6 +2,16 @@ function Contact()
 {
     return(
         <>
+            <div className="form-container">
+                <form className="form">
+                    <div className="name-div">
+                        <input type="text" placeholder="Your name ..." className="name-contact"></input>
+                    </div>
+                    <div className="mail-div">
+                        <input type="mail" placeholder="Your email ..." className="mail-contact"></input>
+                    </div>
+                </form>
+            </div>
         </>
     );
 }
