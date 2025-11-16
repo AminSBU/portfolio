5b6faa63d5b10ec0bc6e8b309d164885cf4197ba
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-11-16 17:25:13 +0330

    textarea and mail input were added into form

--- jsPortfolio/src/Contact.jsx
+++ jsPortfolio/src/Contact.jsx
@@ -10,6 +10,14 @@ function Contact()
                     <div className="mail-div">
                         <input type="mail" placeholder="Your email ..." className="mail-contact"></input>
                     </div>
+                    <div className="mail-div">
+                        <textarea className="message-contact" placeholder="Write your message ..."></textarea>
+                    </div>
+                    <div className="mail-div">
+                        <button className="send-button">Send</button>
+                        <button className="clear-button">Clear</button>
+                    </div>
+
                 </form>
             </div>
         </>
