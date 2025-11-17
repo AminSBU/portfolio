9af7d438cab6333761c597fbd00f915d987bac52
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-11-17 19:06:19 +0330

    handler for clear form, onClick event for clear

--- jsPortfolio/src/Contact.jsx
+++ jsPortfolio/src/Contact.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import './Contact.css'
 
 function Contact()
 {
+    const [name, setName] = useState("");
+    const [mail, setMail] = useState("");
+    const [message, setMessage] = useState("");
+    const [formData, setFormData] = useState("");
+
+    const clearButtonHandler = () => {
+        setFormData("");
+        setName("");
+    }
+
     return(
         <>
             <div className="form-container">
                 <form className="form">
                     <div className="name-div">
-                        <input type="text" placeholder="Your name ..." className="name-contact"></input>
+                        <input type="text" placeholder="Your name ..." className="name-contact" value={name} ></input>
                     </div>
                     <div className="mail-div">
                         <input type="mail" placeholder="Your email ..." className="mail-contact"></input>
@@ -16,7 +27,7 @@ function Contact()
                         <textarea className="message-contact" placeholder="Write your message ..."></textarea>
                     </div>
                     <div className="button-div">
-                        <button className="send-button">Send</button>
+                        <button className="send-button" onClick={clearButtonHandler}>Send</button>
                         <button className="clear-button">Clear</button>
                     </div>
 
